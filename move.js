10e07622509148421603f33fc46fe1fc36561449
Diana Zaqaryan <diana.zaqaryan.92@mail.ru>
2021-09-28 15:44:47 +0400

    delete all texts

--- move.js
+++ move.js
@@ -1,8 +1,7 @@
 alert("hello world");
 <<<<<<< HEAD
 alert("hello from Git000000");
-=======
+
 alert("hello from Git");
 console.log("some");
 console.log("any");
->>>>>>> c8d6cf76986cc959119b27af977fc801208bac7e
